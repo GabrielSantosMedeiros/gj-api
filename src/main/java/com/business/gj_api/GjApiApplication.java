90629f911bdb82ab106e11b02b3442f0252358ef
GabrielSantosMedeiros <gabriel7.gs61@gmail.com>
2025-01-04 19:47:45 -0300

    initial

---
+++ src/main/java/com/business/gj_api/GjApiApplication.java
@@ -0,0 +1,13 @@
+package com.business.gj_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GjApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GjApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/business/gj_api/GjApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.business.gj_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GjApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
