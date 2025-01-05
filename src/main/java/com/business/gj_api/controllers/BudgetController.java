b9f88d03287e6e0e562bf5bddcc1b9749ec9f3f4
GabrielSantosMedeiros <gabriel7.gs61@gmail.com>
2025-01-05 14:05:24 -0300

    create post function for /budget route

--- src/main/java/com/business/gj_api/controllers/BudgetController.java
+++ src/main/java/com/business/gj_api/controllers/BudgetController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,11 +18,16 @@ import com.business.gj_api.repositories.BudgetRepository;
 public class BudgetController {
     
     @Autowired
-    private static BudgetRepository budgetRepository;
+    private BudgetRepository budgetRepository;
 
     @GetMapping
     @ResponseBody
     private List<Budget> getAll() {
         return budgetRepository.findAll();
+    }
+
+    @PostMapping
+    private Budget newBudget(@RequestBody Budget budget) {
+        return budgetRepository.save(budget);
     }
 }
